8d410b344f65d200b466615af0536ef77e5cca2c
Luis Miguel Casado Díaz <luismiguelcasadodiaz@gmail.com>
2022-10-27 22:03:29 +0200

    File analysis into a dictionary Show most frequent words

--- Word_Frequency.py
+++ Word_Frequency.py
@@ -14,20 +14,32 @@ import sys
 import os
 
 def process_file(lines):
-    pass
-def show_frecuencies(dict):
-    pass
+    word_frecuency = {}
+    for line in lines:
+        for palabra in line.strip().split():
+           if palabra not in word_frecuency:
+               word_frecuency.setdefault(palabra,1)
+           else:
+               word_frecuency[palabra] += 1
+
+    return word_frecuency        
+            
+def show_frecuencies(mydict):
+    res = {val[0] : val[1] for val in sorted(mydict.items(), key = lambda x: (x[0], x[1]))}
+
+    for k,v in res.items():
+        print (k,":",v)
+    
 
 if __name__ == "__main__":
-    try:
-        my_directory = os.getcwd()
-        print(my_directory)
-        my_file = os.path.join(my_directory, sys.argv[1])
-        print(my_file)
-        with open(my_file,'r') as f:
-            lines = f.readlines()
-            frecuencies=process_file(lines)
-    except:
-        print("File not found")
+    my_directory = os.getcwd()
+    print(my_directory)
+    my_file = os.path.join(my_directory, sys.argv[1])
+    print(my_file)
+    with open(my_file,encoding='utf-8') as f:
+         lines = f.readlines()
+         frecuencies=process_file(lines)
+         show_frecuencies(frecuencies)
+
         
             
